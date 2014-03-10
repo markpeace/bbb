33f2c5e9acea6da22e760807976c16a42358fc8c
Mark Peace <mark@learnsomestuff.com>
2014-03-10 21:28:03 +0000

    retrieve list of dates

--- www/js/controllers/schedule.js
+++ www/js/controllers/schedule.js
@@ -1,9 +1,22 @@
 bbb.controller('Schedule', function($scope, ParseService) { 
   var Event = Parse.Object.extend("events");
   var query = new Parse.Query(Event);
+  query.ascending("Date");
   query.find({
     success: function(results) {
+      
+      function formatDate(d) {
+        dt= new Date(d);
+        return dt.getDate().toString() + "-" + dt.getMonth().toString() + "-"  + dt.getFullYear().toString()
+      }
+      
       $scope.events=results;
+      $scope.dates=[];
+      t=""
+      for (r in results) {
+        if (formatDate(results[r].get("Date"))!=t) { $scope.dates.push(formatDate(results[r].get("Date"))) }
+        t=formatDate(results[r].get("Date"))
+      }
       $scope.$apply();
     }
   });
