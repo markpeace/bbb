de4ed1b3bde23c81036a915edc2458b11f896dc4
Mark Peace <mark@learnsomestuff.com>
2014-03-24 20:44:51 +0000

    created basic splash screen

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,22 +1,28 @@
 bbb.config(function($stateProvider, $urlRouterProvider) {
-
+  
   $stateProvider
-    .state('tabs', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "pages/tabs.html"
-    })
-    .state('tabs.schedule', {
-      url: "/schedule",
-      views: {
-        'schedule-tab': {
-          templateUrl: "pages/schedule.html",
-          controller: 'Schedule'          
-        }
+  
+  .state('login', {
+    url: "/user/login",
+    templateUrl: "pages/user/login.html"          
+  })
+  
+  .state('tabs', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "pages/tabs.html"
+  })
+  .state('tabs.schedule', {
+    url: "/schedule",
+    views: {
+      'schedule-tab': {
+        templateUrl: "pages/schedule.html",
+        controller: 'Schedule'          
       }
-    });
-
-
-   $urlRouterProvider.otherwise("/tab/schedule");
-
+    }
+  });
+  
+  
+  $urlRouterProvider.otherwise("/user/login");
+  
 })
