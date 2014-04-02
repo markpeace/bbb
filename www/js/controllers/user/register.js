d8e66edb606bd4a91f2c336a86cde9c9eba8a8dc
Mark Peace <mark@learnsomestuff.com>
2014-04-02 10:45:31 +0000

    can now register

--- www/js/controllers/user/register.js
+++ www/js/controllers/user/register.js
@@ -1,15 +1,16 @@
-bbb.controller('Register', function($scope, $ionicModal, ParseService) { 
+bbb.controller('Register', function($scope, $ionicModal,  $ionicLoading, ParseService, $state) { 
   
   $scope.programmes=[];
   
   $scope.programmeLabel="Programme";
   
   $scope.newUser = {
-    username: '',
+    email: '',
     password: '',
     confirm_password: '',
     programme: ''    
   }
+  
   
   var Programmes = Parse.Object.extend("Programmes");
   var query = new Parse.Query(Programmes);
@@ -28,8 +29,62 @@ bbb.controller('Register', function($scope, $ionicModal, ParseService) {
   });  
   
   
-  $scope.$watch('newUser.programme',function(){
+  $scope.$watch('newUser.programme',function(){      
     if ($scope.newUser.programme) { $scope.programmeLabel = $scope.newUser.programme.get('label') } 
   })  
+  
+  $scope.evaluate = {
+    email: function() {
+      if (new RegExp(".*@.*mmu.ac.uk").exec($scope.newUser.email)) { return true; } else { return false; }      
+    },
+    programme: function() {
+      if ($scope.newUser.programme!='') { return true; } else { return false; }      
+    },
+    password: function() {
+      if ($scope.newUser.password.length>5) { return true; } else { return false; }      
+    },
+    confirm_password: function() {
+      if ($scope.newUser.confirm_password.length>0 && $scope.newUser.password==$scope.newUser.confirm_password) { return true; } else { return false; }      
+    }
+  }
+  
+  $scope.signup = function () {
+    
+    errors=''
+    
+    if (!$scope.evaluate.email()) { errors=errors + "- You must register with a valid MMU Email Address \n" }
+    if (!$scope.evaluate.programme()) { errors=errors + "- You must choose an MMU programme of study \n" }    
+    if (!$scope.evaluate.password()) { errors=errors + "- You must choose a password longer than five characters \n" }    
+    if (!$scope.evaluate.confirm_password()) { errors=errors + "- Your password and confirmation should match \n" }
+    
+    if (errors!='') {
+      alert(errors);
+      return false;
+    }
+    
+    $scope.loading = $ionicLoading.show({
+      content: 'Loading',
+    });
+    
+    var user = new Parse.User();
+    user.set("email",$scope.newUser.email)
+    user.set("username",$scope.newUser.email)
+    user.set("password",$scope.newUser.password)
+    
+    user.signUp(null, {
+      success: function(user) {        
+        $scope.currentUser = user;
+        $scope.$apply(); // Notify AngularJS to sync currentUser
+        alert('An email has been sent to allow you to complete your registration')
+        $state.go('login')
+        $scope.loading.hide();
+      },
+      error: function(user, error) {
+        $scope.loading.hide();
+        alert("Unable to sign up:  " + error.code + " " + error.message);
+      }
+    });    
+    
+  }
   
 });
