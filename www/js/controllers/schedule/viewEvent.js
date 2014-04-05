8b4d39c1c39908c1638ed1a5a33efcf90fda7b81
Mark Peace <mark@learnsomestuff.com>
2014-04-05 15:13:49 +0000

    view event finished

--- www/js/controllers/schedule/viewEvent.js
+++ www/js/controllers/schedule/viewEvent.js
@@ -1,11 +1,13 @@
 bbb.controller('ViewEvent', function($scope, ParseService, $rootScope, $stateParams) { 
   
+  $scope.moment=moment
+  
   var Event = Parse.Object.extend("Event");
   var query = new Parse.Query(Event);
   query.include("series").include("host");
   query.get($stateParams.id, {
     success: function(results) {
-      $scope.event=results
+      $scope.event=results      
       $scope.$apply()
     }
   });
