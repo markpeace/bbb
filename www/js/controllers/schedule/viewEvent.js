5d819a51775d4b955424cf7fbaea4778c047ed3b
Mark Peace <mark@learnsomestuff.com>
2014-04-05 14:59:55 +0000

    basic view event

--- www/js/controllers/schedule/viewEvent.js
+++ www/js/controllers/schedule/viewEvent.js
@@ -1,3 +1,13 @@
-bbb.controller('ViewEvent', function($scope, ParseService, $rootScope) { 
+bbb.controller('ViewEvent', function($scope, ParseService, $rootScope, $stateParams) { 
+  
+  var Event = Parse.Object.extend("Event");
+  var query = new Parse.Query(Event);
+  query.include("series").include("host");
+  query.get($stateParams.id, {
+    success: function(results) {
+      $scope.event=results
+      $scope.$apply()
+    }
+  });
   
 });
