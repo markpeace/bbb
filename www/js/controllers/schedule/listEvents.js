c63e254e45af9caab08158816353645aea241dec
Mark Peace <mark@learnsomestuff.com>
2014-04-07 19:11:06 +0000

    functionality to filter booked events

--- www/js/controllers/schedule/listEvents.js
+++ www/js/controllers/schedule/listEvents.js
@@ -3,41 +3,51 @@ bbb.controller('ListEvents', function($state, $scope, ParseService, $rootScope) 
   $scope.moment=moment
   $scope.securityLevel=$rootScope.currentUser.get('securityLevel');
   
-  var Event = Parse.Object.extend("Event");
-  var query = new Parse.Query(Event);       
-  
-  if ($state.current.name!="tabs.schedule") {    
-    var User = $rootScope.currentUser
-    User.relation("bookings").query().find().then(function(result) {
-      bookingId = []
-      for (r in result) { bookingId.push(result[r].id) }
-      console.log(bookingId)
-      query.equalTo("objectId", bookingId)
-    })    
-  }
-  
-  query.ascending("time").include("series").include("host");
-  query.find({
-    success: function(results) {
-      
-      $scope.events=results;
-      $scope.dates=[];
-      
-      t=""
-      for (r in results) {
-        proposed=moment(results[r].get("time")).format("dddd, Do MMMM")
-        if (proposed!=t) { $scope.dates.push(proposed) }
-        t=proposed
-      }
-      
-      $scope.selectedDate=0;
-      
-      $scope.$apply();
-    }
-  });
-  
   $scope.changeDate=function(increment) {
     $scope.selectedDate=$scope.selectedDate+increment;
   }
+  
+  var getAllEvents = function () {
+    var Event = Parse.Object.extend("Event");
+    var query = new Parse.Query(Event);
+    query.ascending("time").include("series").include("host").find().then(function(results) {
+      $scope.events=results
+      findDates();
+    });
+    
+  }
+  
+  var getBookedEvents = function () {
+    $scope.events=[]
+    var User=$rootScope.currentUser
+    User.relation("bookings").query().include("event").include("event.host").include("event.series").find().then(function(result) {
+      eventIds=[]
+      for (r in result) { $scope.events.push(result[r].get("event")) }
+      findDates();
+    })
+  }
+  
+  var findDates = function () {
+    $scope.dates=[];
+    
+    t=""
+    for (r in $scope.events) {
+      proposed=moment($scope.events[r].get("time")).format("dddd, Do MMMM")
+      if (proposed!=t) { $scope.dates.push(proposed) }
+      t=proposed
+    }
+    
+    $scope.selectedDate=0;
+    
+    $scope.$apply();  
+  }
+  
+  if ($state.current.name=="tabs.schedule") { 
+    getAllEvents()
+  } else if($state.current.name=="tabs.schedule_booked") {
+    getBookedEvents()
+  }
+ 
+  
   
 });
