a0fce478d963f10245422562c97f592651471659
Mark Peace <mark@learnsomestuff.com>
2014-04-07 16:35:05 +0000

    failed to set up my events filter

--- www/js/controllers/schedule/listEvents.js
+++ www/js/controllers/schedule/listEvents.js
@@ -3,17 +3,22 @@ bbb.controller('ListEvents', function($state, $scope, ParseService, $rootScope) 
   $scope.moment=moment
   $scope.securityLevel=$rootScope.currentUser.get('securityLevel');
   
-  var Event = Parse.Object.extend("Event");
-  var query = new Parse.Query(Event);
-    
-  console.log($state.current.name)
+  var query =null
   
+  if ($state.current.name=="tabs.schedule") {    
+    var Event = Parse.Object.extend("Event");
+    var query = new Parse.Query(Event);        
+  } else {        
+    var query = $rootScope.currentUser.relation("bookings").query()    
+  }
+   
   query.ascending("time").include("series").include("host");
   query.find({
     success: function(results) {
-      
+           
       $scope.events=results;
       $scope.dates=[];
+            
       t=""
       for (r in results) {
         proposed=moment(results[r].get("time")).format("dddd, Do MMMM")
