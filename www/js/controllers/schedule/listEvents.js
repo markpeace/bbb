9002fdcf700b5ce40d3086e71ec5c828f119c352
Mark Peace <mark@learnsomestuff.com>
2014-04-07 17:23:47 +0000

    failed to implement myevents filter

--- www/js/controllers/schedule/listEvents.js
+++ www/js/controllers/schedule/listEvents.js
@@ -3,22 +3,26 @@ bbb.controller('ListEvents', function($state, $scope, ParseService, $rootScope) 
   $scope.moment=moment
   $scope.securityLevel=$rootScope.currentUser.get('securityLevel');
   
-  var query =null
+  var Event = Parse.Object.extend("Event");
+  var query = new Parse.Query(Event);       
   
-  if ($state.current.name=="tabs.schedule") {    
-    var Event = Parse.Object.extend("Event");
-    var query = new Parse.Query(Event);        
-  } else {        
-    var query = $rootScope.currentUser.relation("bookings").query()    
+  if ($state.current.name!="tabs.schedule") {    
+    var User = $rootScope.currentUser
+    User.relation("bookings").query().find().then(function(result) {
+      bookingId = []
+      for (r in result) { bookingId.push(result[r].id) }
+      console.log(bookingId)
+      query.equalTo("objectId", bookingId)
+    })    
   }
-   
+  
   query.ascending("time").include("series").include("host");
   query.find({
     success: function(results) {
-             
+      
       $scope.events=results;
       $scope.dates=[];
-            
+      
       t=""
       for (r in results) {
         proposed=moment(results[r].get("time")).format("dddd, Do MMMM")
