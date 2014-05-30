28221edefe9fac9ef1564c1c37db8bb0d3f6cbfe
markpeace <mark@learnsomestuff.com>
2014-05-30 11:08:50 +0000

    now generates QR code

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ angular.element(document).ready(function() {
   angular.bootstrap(document, ["bbb"]);
 });
 
-var bbb = angular.module('bbb', ['ionic'])
+var bbb = angular.module('bbb', ['ionic', 'monospaced.qrcode'])
 .run(function($rootScope, ParseService, $location, $state) {
   
   $rootScope.currentUser = Parse.User.current();
