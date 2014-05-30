3d43166d26c536cd78db8ae506225d6e604cc3d8
markpeace <mark@learnsomestuff.com>
2014-05-30 14:01:13 +0000

    added barcode scanner

--- js/controllers/checkin/checkin.js
+++ js/controllers/checkin/checkin.js
@@ -1,15 +1,17 @@
 bbb.controller('CheckIn', function($scope, ParseService) { 
-        
-  cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
-  
+
+        console.log("checkin");
+
+        cordova.plugins.barcodeScanner.scan(
+                function (result) {
+                        alert("We got a barcode\n" +
+                              "Result: " + result.text + "\n" +
+                              "Format: " + result.format + "\n" +
+                              "Cancelled: " + result.cancelled);
+                }, 
+                function (error) {
+                        alert("Scanning failed: " + error);
+                }
+        );
+
 });
