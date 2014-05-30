a1b9368b6eb05984b06bae1fa492807ef5ec57a7
markpeace <mark@learnsomestuff.com>
2014-05-30 12:21:07 +0000

    layed out checkin

--- js/controllers/schedule/eventQRCode.js
+++ js/controllers/schedule/eventQRCode.js
@@ -8,8 +8,6 @@ bbb.controller('EventQRCode', function($scope, $state,  $stateParams,  $ionicMod
         query.include("event").include("event.series").include("host").include("location");
         query.get($stateParams.id, {})
         .then(function (results) {
-                console.log("done")
-                console.log(results);
                 $scope.iteration=results
                 $scope.$apply();                        
         })
