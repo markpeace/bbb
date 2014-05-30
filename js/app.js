82e2e3015fbaae8bf18bb125d266f5ea133b1934
markpeace <mark@learnsomestuff.com>
2014-05-30 14:33:01 +0000

    added barcode scanner

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
+document.addEventListener("deviceready", function() {
+        angular.bootstrap(document, ["bbb"]);
+        console.log("yep")
+}, false);
+
 angular.element(document).ready(function() {
-  angular.bootstrap(document, ["bbb"]);
+  //angular.bootstrap(document, ["bbb"]);
 });
 
 var bbb = angular.module('bbb', ['ionic', 'monospaced.qrcode'])
