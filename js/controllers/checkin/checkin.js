116048fe6f2c73bcc4ba2a16e8633124af695cc9
markpeace <mark@learnsomestuff.com>
2014-06-08 20:14:36 +0000

    trying to get the barcode reader working

--- js/controllers/checkin/checkin.js
+++ js/controllers/checkin/checkin.js
@@ -1,5 +1,5 @@
 bbb.controller('CheckIn', function($scope, $state, ParseService, cordovaCamera) { 
-        
+
         try {
 
                 var scanner = cordova.require("cordova/plugin/BarcodeScanner");
@@ -11,18 +11,29 @@ bbb.controller('CheckIn', function($scope, $state, ParseService, cordovaCamera) 
                         new Parse.Query(Parse.Object.extend("Iteration"))
                         .get(result.text).then(function(iteration) {
 
-                                var Checkin = Parse.Object.extend("Checkin");
-                                checkin = new Checkin(); 
 
-                                checkin.save({
-                                        user: Parse.User.current(),
-                                        iteration: iteration                                       
-                                }).then(function() {
-                                                                            
-                                })                                
+                                new Parse.Query(Parse.Object.extend("Checkin"))
+                                .equalTo("iteration", iteration).equalTo("user",Parse.User.current())
+                                .count().then(function(c) {
+
+                                        if(c==0) {
+
+                                                var Checkin = Parse.Object.extend("Checkin");
+                                                checkin = new Checkin(); 
+
+                                                checkin.save({
+                                                        user: Parse.User.current(),
+                                                        iteration: iteration                                       
+                                                })
+
+                                        }
+                                })
+
+
+
 
                         })
-                        
+
                         $state.go("tabs.schedule")  
 
                 } );
