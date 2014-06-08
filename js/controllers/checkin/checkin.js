2f31c495de4bf29dd75c04246d48ac2acb7e4dca
markpeace <mark@learnsomestuff.com>
2014-06-08 18:58:58 +0000

    messing with barcode scanner

--- js/controllers/checkin/checkin.js
+++ js/controllers/checkin/checkin.js
@@ -9,22 +9,7 @@ bbb.controller('CheckIn', function($scope, ParseService, cordovaCamera) {
 
                 scanner.scan( function (result) { 
 
-                        alert("We got a barcode\n" + 
-                              "Result: " + result.text + "\n" + 
-                              "Format: " + result.format + "\n" + 
-                              "Cancelled: " + result.cancelled);  
-
-                        console.log("Scanner result: \n" +
-                                    "text: " + result.text + "\n" +
-                                    "format: " + result.format + "\n" +
-                                    "cancelled: " + result.cancelled + "\n");
-                        document.getElementById("info").innerHTML = result.text;
-                        console.log(result);
-                        /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-            }
-            */
+                        console.log("scanned")
 
                 }, function (error) { 
                         console.log("Scanning failed: ", error); 
