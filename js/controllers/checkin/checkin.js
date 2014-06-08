d99c2cf5aeaeaf25dc4ee0af0b86e91b8c2cff43
markpeace <mark@learnsomestuff.com>
2014-06-08 18:53:17 +0000

    messing with barcode scanner

--- js/controllers/checkin/checkin.js
+++ js/controllers/checkin/checkin.js
@@ -3,18 +3,34 @@ bbb.controller('CheckIn', function($scope, ParseService, cordovaCamera) {
         console.log("checkin");
         try {
 
+                console.log('scanning');
 
-                cordova.plugins.barcodeScanner.scan(
-                        function (result) {
-                                alert("We got a barcode\n" +
-                                      "Result: " + result.text + "\n" +
-                                      "Format: " + result.format + "\n" +
-                                      "Cancelled: " + result.cancelled);
-                        }, 
-                        function (error) {
-                                alert("Scanning failed: " + error);
-                        }
-                );
+                var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+                scanner.scan( function (result) { 
+
+                        alert("We got a barcode\n" + 
+                              "Result: " + result.text + "\n" + 
+                              "Format: " + result.format + "\n" + 
+                              "Cancelled: " + result.cancelled);  
+
+                        console.log("Scanner result: \n" +
+                                    "text: " + result.text + "\n" +
+                                    "format: " + result.format + "\n" +
+                                    "cancelled: " + result.cancelled + "\n");
+                        document.getElementById("info").innerHTML = result.text;
+                        console.log(result);
+                        /*
+            if (args.format == "QR_CODE") {
+                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            }
+            */
+
+                }, function (error) { 
+                        console.log("Scanning failed: ", error); 
+                } );
+
+
 
         } catch (ex) {
                 console.log("error" + ex)
