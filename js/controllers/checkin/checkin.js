211602cf4a54a1b299e118ec4f7ff4d080119c93
markpeace <mark@learnsomestuff.com>
2014-06-08 19:54:10 +0000

    trying to get the barcode reader working

--- js/controllers/checkin/checkin.js
+++ js/controllers/checkin/checkin.js
@@ -6,13 +6,18 @@ bbb.controller('CheckIn', function($scope, ParseService, cordovaCamera) {
                 var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
                 scanner.scan( function (result) { 
-                        
+
                         console.log("scanned")
 
                         new Parse.Query(Parse.Object.extend("Iteration"))
                         .get(result.text).then(function(iteration) {
-                                console.log("processed")
-                                console.log(iteration)
+                                
+                                new (Parse.Object.extend("Checkin"))
+                                .save({
+                                        user: Parse.User.current(),
+                                        iteration: iteration                                       
+                                })
+                                
                         })
 
                 } );
