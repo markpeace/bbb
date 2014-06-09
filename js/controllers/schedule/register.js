bb1a205307fcad66762c29c09d0bc71d7b17e785
markpeace <mark@learnsomestuff.com>
2014-06-09 20:33:28 +0000

    can manually check in students

--- js/controllers/schedule/register.js
+++ js/controllers/schedule/register.js
@@ -1,6 +1,6 @@
 bbb.controller('AttendenceRegister', function($scope, ParseService, $stateParams) { 
 
-        $scope.attendees = ["M"]
+        $scope.bookings = ["M"]
         $scope.id=$stateParams.id
         
         $scope.iterations =[]
@@ -12,10 +12,35 @@ bbb.controller('AttendenceRegister', function($scope, ParseService, $stateParams
         new Parse.Query("Booking")
         .equalTo("iteration", dummyIteration)
         .include("user")
+        .include("checkin")
         .find().then(function(result) {
-                $scope.attendees = result
+                $scope.bookings = result
                 $scope.$apply()
         })
                 
 
+        $scope.toggleAttendence = function(booking) {
+                                
+                if(booking.get("checkin")) {
+                        booking.get("checkin").destroy().then(function() {
+                                booking.set("checkin", null).save().then(function() {
+                                        $scope.$apply()
+                                })
+                        })
+                } else {
+                        checkin = new Parse.Object("Checkin")
+                        checkin.save({
+                                booking:booking,
+                                user: Parse.User.current()
+                        }).then(function (result) {
+                                booking.set("checkin", result).save().then(function() {
+                                        $scope.$apply()
+                                })
+                                
+                        })                        
+                        
+                }
+        }
+        
+        
 });
