3d06d3e8cbd7a56269d022df3bc0971438fb81b3
markpeace <mark@learnsomestuff.com>
2014-06-09 20:16:07 +0000

    attendence register now shows

--- js/controllers/schedule/register.js
+++ js/controllers/schedule/register.js
@@ -1,5 +1,21 @@
 bbb.controller('AttendenceRegister', function($scope, ParseService, $stateParams) { 
+
+        $scope.attendees = ["M"]
+        $scope.id=$stateParams.id
         
-        console.log("AttendenceRegister")
+        $scope.iterations =[]
+
+
+        dummyIteration = new Parse.Object("Iteration")
+        dummyIteration.id = $stateParams.id
+
+        new Parse.Query("Booking")
+        .equalTo("iteration", dummyIteration)
+        .include("user")
+        .find().then(function(result) {
+                $scope.attendees = result
+                $scope.$apply()
+        })
+                
 
 });
