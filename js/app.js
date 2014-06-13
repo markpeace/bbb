77e52d37fd7ef03b0d5d1c5959a451a80512938e
markpeace <mark@learnsomestuff.com>
2014-06-13 17:28:14 +0000

    messing with state handler

--- js/app.js
+++ js/app.js
@@ -3,20 +3,30 @@ if (typeof cordova === 'object') {
                 angular.bootstrap(document, ["bbb"]);
                 console.log("cordova intialised!")
 
-                try {
+                document.addEventListener("deviceready", init, false);
 
-                        console.log("setup listener")
+                function init() {
+                        document.addEventListener("online", toggleCon, false);
+                        document.addEventListener("offline", toggleCon, false);
 
-                        document.addEventListener("offline", onOffline, false);
-
-                        function onOffline() {
-                                alert($state.current.name)
-                                console.log("mark")
-                                console.log($state.current.name)
+                        if(navigator.network.connection.type == Connection.NONE) {
+                                navigator.notification.alert("Sorry, you are offline.", function() {}, "Offline!");
+                        } else {
+                                setupButtonHandler();
                         }
 
-                } catch (ex) {
-                        console.log(ex)
+                }
+
+                function toggleCon(e) {
+                        console.log("Called",e.type);
+                        if(e.type == "offline") {
+                                $("#searchBtn").off("touchstart").attr("disabled","disabled");
+                                navigator.notification.alert("Sorry, you are offline.", function() {}, "Offline!");
+                        } else {
+                                $("#searchBtn").removeAttr("disabled");
+                                navigator.notification.alert("Woot, you are back online.", function() {}, "Online!");
+                                setupButtonHandler();
+                        }
                 }
 
 
