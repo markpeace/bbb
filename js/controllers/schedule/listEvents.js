463fcefd2c933ac0235ee6e7bf0a9411d2706584
markpeace <mark@learnsomestuff.com>
2014-07-02 20:00:41 +0000

    events are now cached

--- js/controllers/schedule/listEvents.js
+++ js/controllers/schedule/listEvents.js
@@ -1,56 +1,38 @@
 bbb.controller('ListEvents', function($state, $scope, ParseService, $rootScope, EventModel) { 
 
+
+
         $scope.moment=moment
         $scope.securityLevel=$rootScope.currentUser.get('securityLevel')
-        $scope.iterations=[]
-        
+
+        $scope.data=EventModel.data();       
+        $scope.selectedDate=0;
+
+        console.log($scope.data)
+
+        $scope.filterByDate = function(items) {
+
+                r=[]
+
+
+                angular.forEach(items, function(item) {
+                        if(moment(item.time).format("dddd, Do MMMM")==$scope.data.dates[$scope.selectedDate]) { r.push(item) }
+                })
+
+                return r;
+        }
+
         $scope.changeDate=function(increment) {
                 $scope.selectedDate=$scope.selectedDate+increment;
         }
 
-        var getAllEvents = function () {
-                var Iteration = Parse.Object.extend("Iteration")
-                var query = new Parse.Query(Iteration);
-                query.ascending("time").include("event").include("host").include("event.series")
-                .find().then(function (results) {
-                        $scope.iterations=results
-                        findDates();
-                })       
-
-        }
-
-        var getBookedEvents = function () {
-                
-                $rootScope.currentUser.relation("bookings").query().include("iteration").include("iteration.event")
-                .include("iteration.host").include("iteration.event.series").find().then(function (result) {
-                        console.log(result)
-                        angular.forEach (result, function (r) { $scope.iterations.push(r.get("iteration")) })
-                        findDates();
-                })
-                              
-        }
-
-        var findDates = function () {
-                $scope.dates=[];
-
-                t=""
-                angular.forEach($scope.iterations, function (event) {
-                        proposed=moment(event.get("time")).format("dddd, Do MMMM")
-                        if (proposed!=t) { $scope.dates.push(proposed) }
-                        t=proposed
-                })
-                
-                $scope.selectedDate=0
-                $scope.$apply()
-        }
-        
 
         if ($state.current.name=="tabs.schedule") { 
                 $scope.scheduleTabColour='rgba(247, 200, 0, 0.5)'
-                getAllEvents()
+                //getAllEvents()
         } else if($state.current.name=="tabs.schedule_booked") {
                 $scope.bookedTabColour='rgba(247, 200, 0, 0.5)'
-                getBookedEvents()
+                //getBookedEvents()
         }
 
 
--- js/services/eventModel.js
+++ js/services/eventModel.js
@@ -1,7 +1,11 @@
-bbb.service('EventModel', [function(ParseService) {                      
+bbb.factory('EventModel', ["ParseService", "$rootScope", function(ParseService, $rootScope) {                      
 
         if(localStorage.getItem(Parse.User.current().id)) { 						// <- Needs removing when we go live...
-                localStorage.setItem(Parse.User.current().id, JSON.stringify({lastUpdated:moment().subtract('years',1)._d})) 
+                localStorage.setItem(Parse.User.current().id, JSON.stringify({
+                        lastUpdated:moment().subtract('years',1)._d,
+                        iterations: [],
+                        dates:[]
+                })) 
                 console.log("Created localStorage Item")
         }
 
@@ -21,20 +25,21 @@ bbb.service('EventModel', [function(ParseService) {
         }
 
 
-        var updateData = function () {
+        var updateData = function () {                
 
                 toLookUp = [
                         { table: "User", constraints: [".lessThan('securityLevel', 3)"], fields: ["forename", "surname", "blurb"] },
                         { table: "Location", constraints: [], fields: ["label", "blurb"] },
                         { table: "Event", constraints: [], fields: ["description", "series", "title"] },
-                        { table: "Iteration", constraints: [], fields: ["capacity", "event", "location"] },
-                        //{ table: "Booking", constraints: [".equalTo('User', "+Parse.User().current.id+")"], fields: ["iteration"] }
+                        { table: "Series", constraints: [], fields: ["label"] },
+                        { table: "Iteration", constraints: [".ascending('time')"], fields: ["capacity", "event", "location", "host", "time"] }
+                        //{ table: "Booking", constraints: [".equalTo('User', "+Parse.User().current().id+")"], fields: ["iteration"] }
                 ]
 
                 lookupIndex=0
 
                 var performLookup = function () {
-                        
+
                         lookupItem = toLookUp[lookupIndex]
 
                         cache.data[lookupItem.table] = []
@@ -49,17 +54,18 @@ bbb.service('EventModel', [function(ParseService) {
                                                 if (newItem[field].id) { newItem[field]=newItem[field].id }
                                         })
                                         cache.data[lookupItem.table][result.id]=newItem
+                                        cache.data[lookupItem.table][result.id].id=result.id
                                 })
-                                
+
                                 console.log("updated " + lookupItem.table)
-                                
+
                                 lookupIndex++
                                 if (lookupIndex<toLookUp.length) { 
-                                	performLookup()
+                                        performLookup()
                                 } else {
                                         weaveData();
                                 }
-                                
+
                         })
 
                 }
@@ -73,12 +79,39 @@ bbb.service('EventModel', [function(ParseService) {
 
         var weaveData= function() {
 
-                           console.log(cache.data)
-                           
-                console.log("updatedEvents")
+
+                iterations = []
+                dates=[]
+
+                for(var objectId in cache.data.Event	) {
+                        cache.data.Event[objectId].series = cache.data.Series[cache.data.Event[objectId].series]
+                }            
+
+                for(var objectId in cache.data.Iteration	) {
+                        cache.data.Iteration[objectId].event = cache.data.Event[cache.data.Iteration[objectId].event]
+                        cache.data.Iteration[objectId].location = cache.data.Location[cache.data.Iteration[objectId].location]
+                        cache.data.Iteration[objectId].host = cache.data.User[cache.data.Iteration[objectId].host]
+                        iterations.push(cache.data.Iteration[objectId])
+
+                        if (moment(cache.data.Iteration[objectId].time).format("dddd, Do MMMM")!=dates[dates.length-1]) {
+                                dates.push(moment(cache.data.Iteration[objectId].time).format("dddd, Do MMMM"))
+                        }
+                }
+
+
+                cache.data.iterations=iterations;
+                cache.data.dates=dates
+
                 cache.data.lastUpdated=moment()._d;                
                 cache.save();
+
+                $rootScope.$apply()
+
+        }
+
+        return {
+                data: function() { return cache.data }
         }
 
 
-        }]);
+}]);
