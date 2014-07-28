70c3adb3a9fbe5ae814075ddf76e7074e5f2b86e
markpeace <mark@learnsomestuff.com>
2014-07-28 08:02:17 +0000

    better functionality

--- pu.sh
+++ pu.sh
@@ -1,4 +1,12 @@
-git add .
-git commit -a -m "$*"
-git clean -f
-git push origin master
+if [ "$1" = "deploy" ] 
+then
+	git checkout master
+        git merge --no-ff development
+        git push origin master
+        git checkout development
+else
+	git add .
+	git commit -a -m "$*"
+	git clean -f
+	git push origin development
+fi
