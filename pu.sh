f37c56786c93706be679f5bd059daeb1db949b19
markpeace <mark@learnsomestuff.com>
2014-07-30 19:55:06 +0000

    fixed script

--- pu.sh
+++ pu.sh
@@ -1,14 +1,3 @@
-if [ "$1" = "deploy" ] 
-then
-	git add .
-        git commit -a -m "tidying commits"
-        git push origin development
-	git checkout master
-        git merge --no-ff development
-        git push origin master
-        git checkout development
-else
-	git add .
-	git commit -a -m "$*"
-	git push origin development
-fi
+git add .
+git commit -a -m "$*"
+git push origin development
