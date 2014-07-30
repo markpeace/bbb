846ba6d9221f4e6a9a90265b0532d0f658131b03
markpeace <mark@learnsomestuff.com>
2014-07-30 19:37:11 +0000

    testing

--- pu.sh
+++ pu.sh
@@ -1,5 +1,8 @@
 if [ "$1" = "deploy" ] 
 then
+	git add .
+        git commit -a -m "tidying commits"
+        git push origin development
 	git checkout master
         git merge --no-ff development
         git push origin master
@@ -7,6 +10,5 @@ then
 else
 	git add .
 	git commit -a -m "$*"
-	git clean -f
 	git push origin development
 fi
