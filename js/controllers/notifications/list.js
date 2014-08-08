e8e251bc9795bfaa7b495482f8353a2367791a16
markpeace <mark@learnsomestuff.com>
2014-08-08 14:15:59 +0000

    messing with local notifications

--- js/controllers/notifications/list.js
+++ js/controllers/notifications/list.js
@@ -9,22 +9,23 @@ bbb.controller('ListNotifications', function($state, $scope, ParseService) {
                 { header:"Header", text: "text goes here" }               
         ]
 
+        alert(moment().add("second",10))
 
         try {
-        window.plugin.notification.local.add({
-                id:         "test",  // A unique id of the notifiction
-                date:       moment().add("second",10),    // This expects a date object
-                message:    "hello this is a test",  // The message that is displayed
-                title:      "test notifier",  // The title of the message
-                //repeat:     String,  // Either 'secondly', 'minutely', 'hourly', 'daily', 'weekly', 'monthly' or 'yearly'
-                badge:      2,  // Displays number badge to notification
-                //sound:      String,  // A sound to be played
-                //json:       String,  // Data to be passed through the notification
-                //autoCancel: Boolean, // Setting this flag and the notification is automatically canceled when the user clicks it
-                //ongoing:    Boolean, // Prevent clearing of notification (Android only)
-        }, function () {
-                alert("this is the callback")
-        });
+                window.plugin.notification.local.add({
+                        id:         "localnotificationtest",  // A unique id of the notifiction
+                        date:       moment().add("second",10),    // This expects a date object
+                        message:    "Local Notification Test",  // The message that is displayed
+                        title:      "This is a test of the local notifications",  // The title of the message
+                        //repeat:     String,  // Either 'secondly', 'minutely', 'hourly', 'daily', 'weekly', 'monthly' or 'yearly'
+                        badge:      2,  // Displays number badge to notification
+                        //sound:      String,  // A sound to be played
+                        //json:       String,  // Data to be passed through the notification
+                        //autoCancel: Boolean, // Setting this flag and the notification is automatically canceled when the user clicks it
+                        //ongoing:    Boolean, // Prevent clearing of notification (Android only)
+                }, function () {
+                        alert("this is the callback")
+                });
         } catch (ex) {
                 alert(ex)
         }
