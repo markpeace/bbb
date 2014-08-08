d102f26719abdff267a51ea335f68679ebb04983
markpeace <mark@learnsomestuff.com>
2014-08-08 14:10:46 +0000

    messing with local notifications

--- js/controllers/notifications/list.js
+++ js/controllers/notifications/list.js
@@ -1,5 +1,5 @@
 bbb.controller('ListNotifications', function($state, $scope, ParseService) { 
-	$scope.dummyData=[
+        $scope.dummyData=[
                 { header:"Header", text: "text goes here" },
                 { header:"Header", text: "text goes here" },
                 { header:"Header", text: "text goes here" },
@@ -8,4 +8,20 @@ bbb.controller('ListNotifications', function($state, $scope, ParseService) {
                 { header:"Header", text: "text goes here" },
                 { header:"Header", text: "text goes here" }               
         ]
+
+
+        window.plugin.notification.local.add({
+                id:         "test",  // A unique id of the notifiction
+                date:       moment().add("second",10),    // This expects a date object
+                message:    "hello this is a test",  // The message that is displayed
+                title:      "test notifier",  // The title of the message
+                //repeat:     String,  // Either 'secondly', 'minutely', 'hourly', 'daily', 'weekly', 'monthly' or 'yearly'
+                badge:      2,  // Displays number badge to notification
+                //sound:      String,  // A sound to be played
+                //json:       String,  // Data to be passed through the notification
+                //autoCancel: Boolean, // Setting this flag and the notification is automatically canceled when the user clicks it
+                //ongoing:    Boolean, // Prevent clearing of notification (Android only)
+        }, callback, scope);
+
+
 });
