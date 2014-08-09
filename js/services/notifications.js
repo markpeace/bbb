ffc79b4583b7acdf6afa690f1da9138c217fe43c
markpeace <mark@learnsomestuff.com>
2014-08-09 10:20:09 +0000

    created notifications service

--- js/services/notifications.js
+++ js/services/notifications.js
@@ -1,9 +1,21 @@
 bbb.factory('NotificationService', ["ParseService", "$ionicLoading","$rootScope","$state", function(ParseService, $ionicLoading, $rootScope, $state) {                      
 
+        newNotifications=10
+
         if (window.plugin) {
                 window.plugin.notification.local.onclick = function (id, state, json) {
                         alert("the user clickified the notification: "+id)
                 };
+
+                window.plugin.notification.local.ontrigger = function (id, state, json) {
+
+                        if (json) window.plugin.notification.local.add({
+                                id:         "badge",  // A unique id of the notifiction
+                                badge:      100,  // Displays number badge to notification
+                        });
+
+                };
+
         }
 
         return {
@@ -34,7 +46,7 @@ bbb.factory('NotificationService', ["ParseService", "$ionicLoading","$rootScope"
                                 date:       _10_seconds_from_now,    // This expects a date object
                                 message:    "A pop-up you are booked into ("+ iteration.event.title +") starts in ten minutes",  // The message that is displayed
                                 badge:      1,  // Displays number badge to notification
-                                //json:       String,  // Data to be passed through the notification
+                                json:       { mark: "peace" },  // Data to be passed through the notification
                         });
 
                 }
