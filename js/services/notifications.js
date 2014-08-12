2d84dc9849f5e3c5deded280bd01d8d9b24141b1
markpeace <mark@learnsomestuff.com>
2014-08-12 13:48:38 +0000

    trying to connect notifications and reminders

--- js/services/notifications.js
+++ js/services/notifications.js
@@ -1,6 +1,6 @@
 bbb.factory('NotificationService', ["$rootScope", "$state", function($rootScope, $state) {                      
     	
-        var _notifier = window.plugin.notification.local
+        var _notifier = window.plugin.notification.local()
         
         var _hookUpEventListeners = function () {
                 alert("wiredup")
