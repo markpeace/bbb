2553ed835ad22bcb20066feaf96254e088026c6d
markpeace <mark@learnsomestuff.com>
2014-08-13 19:47:29 +0000

    beginning functionality for sending messages

--- js/controllers/schedule/messageAttendees.js
+++ js/controllers/schedule/messageAttendees.js
@@ -1,3 +1,22 @@
-bbb.controller('MessageAttendees', function($scope, $state, ParseService) { 
-	console.log("messageAttendees")
+bbb.controller('MessageAttendees', function($scope, $state, $stateParams, ParseService) { 
+        $scope.id=$stateParams.id
+
+        dummyIteration = (new (Parse.Object.extend("Booking")))
+        dummyIteration.id = $scope.id                        
+
+
+        new Parse.Query("Booking")
+        .equalTo("iteration", dummyIteration)
+        .include("user")
+        .find().then(function(result) {
+                
+                tokens=[]
+                
+       		angular.forEach(result, function(r) {
+                        tokens.push(r.get("user").get("token"))
+                })      
+                
+                console.log(tokens)
+        })
+
 })        
