23516b7b80c3f5368929c4b86f4b5b0d8d7e99f8
markpeace <mark@learnsomestuff.com>
2014-08-13 11:36:40 +0000

    fixed scheduled remindes

--- js/services/notifications.js
+++ js/services/notifications.js
@@ -89,20 +89,19 @@ bbb.factory('NotificationService', ["$rootScope", "$state", "$location", functio
 
                                 iteration.time=moment().add('minutes', 10)
                                 
-                                i=iteration.time.subtract('minutes',10).add('seconds',60)._d 
+                                i=iteration.time.subtract('minutes',10).add('seconds',20)._d 
 
                                       var now                  = new Date().getTime(),
-                                        _60_seconds_from_now = new Date(now + 60*1000);
+                                        _20_seconds_from_now = new Date(now + 20*1000);
 
-                                console.log(i)
-                                console.log(_60_seconds_from_now)
+                                if(i!=_20_seconds_from_now) { alert("BAD") }
                                 
 
                                 if(window.plugin) {                                          
 
                                         window.plugin.notification.local.add({
                                                 id:         iteration.id,  // A unique id of the notifiction
-                                                date:      iteration.time.subtract('minutes',10).add('seconds',60)._d,
+                                                date:      i,
                                                 message:    "A pop-up you are booked into ("+ iteration.event.title +") starts in ten minutes",  // The message that is displayed
                                                 json:       JSON.stringify({ 
                                                         "title":"Event Reminder!",
