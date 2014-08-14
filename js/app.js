c1bbc91db970c4addd6deb5f26bb613ecd44fd80
markpeace <mark@learnsomestuff.com>
2014-08-14 11:27:20 +0000

    messing with notifications

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 if (typeof cordova === 'object') {
         document.addEventListener("deviceready", function() {
-                angular.bootstrap(document, ["bbb"]);                
+                angular.bootstrap(document, ["bbb"]);        
+
+                var pushNotification = window.plugins.pushNotification;
+                pushNotification.registerDevice({alert:true, badge:true, sound:true}, function(status) {
+                        app.myLog.value+=JSON.stringify(['registerDevice status: ', status])+"\n";
+                        app.storeToken(status.deviceToken);
+                });
+
+                pushNotification.getPendingNotifications(function(notifications) {
+                        app.myLog.value+=JSON.stringify(['getPendingNotifications', notifications])+"\n";
+                        console.log(JSON.stringify(['getPendingNotifications', notifications]));
+                });
+
+                pushNotification.getRemoteNotificationStatus(function(status) {
+                        app.myLog.value+=JSON.stringify(['Registration check - getRemoteNotificationStatus', status])+"\n";
+                });
+
+
         }, false);
 
 } else {        
