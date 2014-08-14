cdb1980fdc8b22b64a0e634daff52701d589bfc4
markpeace <mark@learnsomestuff.com>
2014-08-14 20:37:06 +0000

    messing with messaging

--- js/app.js
+++ js/app.js
@@ -1,83 +1,42 @@
 if (typeof cordova === 'object') {
         document.addEventListener("deviceready", function() {
-                
+
                 angular.bootstrap(document, ["bbb"]);        
 
-                var PushNotApp = PushNotApp || {};
- 
-PushNotApp.main = (function() {
- 
- var pushNotification = window.plugins.pushNotification,
- 
- showAlert = function(message, title) {
-  if(navigator.notification) {
-   navigator.notification.alert(message, null, title, 'Close');
-   navigator.notification.vibrate(1000);
-  }else{
-   alert(title ? (title + ": " + message) : message);
-  }
- },
- 
- addCallback = function(key, callback) {
-  if(window.callbacks === undefined) {
-   window.callbacks = {};
-  }
-  window.callbacks[key] = callback;
- },
- 
- addNotification = function(notificationTxt) {
-  console.log('notification added to DOM');
-  var el = document.getElementById('notification');
-  el.innerHTML += notificationTxt;
- },
- 
- registrationSuccessHandler = function(token) {
-  console.log('successful registration with token: ' + token);
-  addCallback('notificationHandler', notificationHandler);
- },
- 
- registrationFailedHandler = function(error) {
-  showAlert(error, "Error");
- },
- 
- notificationHandler = function(evt) {
-  console.log("received a notification: " + evt.alert);
-  navigator.notification.beep(3);
-  if(evt.alert) {
-   addNotification(evt.alert);
-  }
-  if(evt.prop){
-   addNotification(" received a special property: " + evt.prop);
-  }
- },
- 
-deviceReady = function() {
- console.log('Device is ready');
- if(parseFloat(device.version) === 7.0) {
-  document.body.style.marginTop = "20px";
- }
- pushNotification.register(registrationSuccessHandler,
-                           registrationFailedHandler, {
-                           "badge":"true",
-                           "sound":"true",
-                           "alert":"true",
-                           "ecb":"callbacks.notificationHandler"
-                           });
-},
- 
-initialize = function(){
- document.addEventListener("deviceready", deviceReady, false);
-}
- 
-return {
- initialize:initialize
-}
- 
-}());
- 
-PushNotApp.main.initialize();
+                function onNotification() {
+                        alert("hi")
+                }
+                
+                var _pushNotifications = {
+                        initialise: function() {
+                                
+                                console.log("Init1")
+                                
+                                window.plugins.pushNotification.register(
+                                        _pushNotifications.tokenHandler,
+                                        _pushNotifications.errorHandler,
+                                        {
+                                                "badge":"true",
+                                                "sound":"true",
+                                                "alert":"true",
+                                                "ecb":"onNotification"
+                                        });
 
-        }, false);
+                        },
+                        successHandler: function() {
+                                alert("success")
+                        },
+                        errorHandler: function() {
+                                alert("error")
+                        },
+                        tokenHandler: function(result) {
+                                alert(result)                
+                        }
+                }        
+
+                _pushNotifications.initialise();
+
+                }, false);
 
 } else {        
         angular.element(document).ready(function() {
