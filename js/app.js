2f9a2df0a641f5288f0eceed7b9b6156b20dfe68
markpeace <mark@learnsomestuff.com>
2014-08-14 20:28:32 +0000

    messing with messaging

--- js/app.js
+++ js/app.js
@@ -1,129 +1,81 @@
-function onDeviceReady() {
-        $("#app-status-ul").append('<li>deviceready event received</li>');
-
-        document.addEventListener("backbutton", function(e)
-                                  {
-                                          $("#app-status-ul").append('<li>backbutton event received</li>');
-
-                                          if( $("#home").length > 0)
-                                          {
-                                                  // call this to get a new token each time. don't call it to reuse existing token.
-                                                  //pushNotification.unregister(successHandler, errorHandler);
-                                                  e.preventDefault();
-                                                  navigator.app.exitApp();
-                                          }
-                                          else
-                                          {
-                                                  navigator.app.backHistory();
-                                          }
-                                  }, false);
-
-        try 
-        { 
-                pushNotification = window.plugins.pushNotification;
-                if (device.platform == 'android' || device.platform == 'Android') {
-                        $("#app-status-ul").append('<li>registering android</li>');
-                        pushNotification.register(successHandler, errorHandler, {"senderID":"599322667326","ecb":"onNotificationGCM"});     // required!
-                } else {
-                        $("#app-status-ul").append('<li>registering iOS</li>');
-                        pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});    // required!
-                }
-        }
-        catch(err) 
-        { 
-                txt="There was an error on this page.\n\n"; 
-                txt+="Error description: " + err.message + "\n\n"; 
-                alert(txt); 
-        } 
-}
-
-// handle APNS notifications for iOS
-function onNotificationAPN(e) {
-        if (e.alert) {
-                $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
-                navigator.notification.alert(e.alert);
-        }
-
-        if (e.sound) {
-                var snd = new Media(e.sound);
-                snd.play();
-        }
-
-        if (e.badge) {
-                pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
-        }
-}
-
-// handle GCM notifications for Android
-function onNotificationGCM(e) {
-        $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
-
-        switch( e.event )
-        {
-                case 'registered':
-                        if ( e.regid.length > 0 )
-                        {
-                                $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-                                // Your GCM push server needs to know the regID before it can push to this device
-                                // here is where you might want to send it the regID for later use.
-                                console.log("regID = " + e.regid);
-                        }
-                        break;
-
-                case 'message':
-                        // if this flag is set, this notification happened while we were in the foreground.
-                        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-                        if (e.foreground)
-                        {
-                                $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
-
-                                // if the notification contains a soundname, play it.
-                                var my_media = new Media("/android_asset/www/"+e.soundname);
-                                my_media.play();
-                        }
-                        else
-                        {   // otherwise we were launched because the user touched a notification in the notification tray.
-                                if (e.coldstart)
-                                        $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
-                                else
-                                        $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-                        }
-
-                        $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-                        $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-                        break;
-
-                case 'error':
-                        $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
-                        break;
-
-                default:
-                        $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-                        break;
-        }
-}
-
-function tokenHandler (result) {
-        $("#app-status-ul").append('<li>token: '+ result +'</li>');
-        alert("yep: " + result)
-        // Your iOS push server needs to know the token before it can push to this device
-        // here is where you might want to send it the token for later use.
-}
-
-function successHandler (result) {
-        $("#app-status-ul").append('<li>success:'+ result +'</li>');
-}
-
-function errorHandler (error) {
-        $("#app-status-ul").append('<li>error:'+ error +'</li>');
-}
-
-document.addEventListener('deviceready', onDeviceReady, true);
-
 if (typeof cordova === 'object') {
         document.addEventListener("deviceready", function() {
-
+                
                 angular.bootstrap(document, ["bbb"]);        
+
+                var PushNotApp = PushNotApp || {};
+ 
+PushNotApp.main = (function() {
+ 
+ var pushNotification = window.plugins.pushNotification,
+ 
+ showAlert = function(message, title) {
+  if(navigator.notification) {
+   navigator.notification.alert(message, null, title, 'Close');
+   navigator.notification.vibrate(1000);
+  }else{
+   alert(title ? (title + ": " + message) : message);
+  }
+ },
+ 
+ addCallback = function(key, callback) {
+  if(window.callbacks === undefined) {
+   window.callbacks = {};
+  }
+  window.callbacks[key] = callback;
+ },
+ 
+ addNotification = function(notificationTxt) {
+  console.log('notification added to DOM');
+  var el = document.getElementById('notification');
+  el.innerHTML += notificationTxt;
+ },
+ 
+ registrationSuccessHandler = function(token) {
+  console.log('successful registration with token: ' + token);
+  addCallback('notificationHandler', notificationHandler);
+ },
+ 
+ registrationFailedHandler = function(error) {
+  showAlert(error, "Error");
+ },
+ 
+ notificationHandler = function(evt) {
+  console.log("received a notification: " + evt.alert);
+  navigator.notification.beep(3);
+  if(evt.alert) {
+   addNotification(evt.alert);
+  }
+  if(evt.prop){
+   addNotification(" received a special property: " + evt.prop);
+  }
+ },
+ 
+deviceReady = function() {
+ console.log('Device is ready');
+ if(parseFloat(device.version) === 7.0) {
+  document.body.style.marginTop = "20px";
+ }
+ pushNotification.register(registrationSuccessHandler,
+                           registrationFailedHandler, {
+                           "badge":"true",
+                           "sound":"true",
+                           "alert":"true",
+                           "ecb":"callbacks.notificationHandler"
+                           });
+},
+ 
+initialize = function(){
+ document.addEventListener("deviceready", deviceReady, false);
+}
+ 
+return {
+ initialize:initialize
+}
+ 
+}());
+ 
+PushNotApp.main.initialize();
 
         }, false);
 
