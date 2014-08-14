0bbd02825b90e639d0d679c3e2a4a0ab3fe95eeb
markpeace <mark@learnsomestuff.com>
2014-08-14 11:44:32 +0000

    trying to get messaging working

--- js/app.js
+++ js/app.js
@@ -2,19 +2,93 @@ if (typeof cordova === 'object') {
         document.addEventListener("deviceready", function() {
                 angular.bootstrap(document, ["bbb"]);        
 
-                var pushNotification = window.plugins.pushNotification;
-                pushNotification.registerDevice({alert:true, badge:true, sound:true}, function(status) {
-                        alert("success:" + status.deviceToken)
-                        
-                });
+	alert("updated")
+                
+                function successHandler (result) {
+                        alert('result = ' + result);
+                }
 
-                pushNotification.getPendingNotifications(function(notifications) {
-                        alert(JSON.stringify(['getPendingNotifications', notifications]));
-                });
+                function errorHandler (error) {
+                        alert('error = ' + error);
+                }
 
-                pushNotification.getRemoteNotificationStatus(function(status) {
-                        alert(JSON.stringify(['Registration check - getRemoteNotificationStatus', status])+"\n");
-                });
+                function onNotificationAPN (event) {
+                        if ( event.alert )
+                        {
+                                navigator.notification.alert(event.alert);
+                        }               
+
+                        if ( event.badge )
+                        {
+                                pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+                        }
+                }
+
+                function onNotification(e) {
+                        $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+                        switch( e.event )
+                        {
+                                case 'registered':
+                                        if ( e.regid.length > 0 )
+                                        {                                               
+                                                alert("regID = " + e.regid);
+                                        }
+                                        break;
+
+                                case 'message':
+                                        // if this flag is set, this notification happened while we were in the foreground.
+                                        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+                                        if ( e.foreground )
+                                        {
+                                               alert("inline notifcation")                                                
+                                        }
+                                        else
+                                        {  // otherwise we were launched because the user touched a notification in the notification tray.
+                                                if ( e.coldstart )
+                                                {
+							console.log("coldstart notification")
+                                                }
+                                                else
+                                                {
+							console.log("background notification")
+                                                }
+                                        }
+
+                                        alert(e.payload.message + e.payload.msgcnt)
+                                        
+                                        break;
+
+                                case 'error':
+                                        alert('<li>ERROR -> MSG:' + e.msg + '</li>');
+                                        break;
+
+                                default:
+                                        alert('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+                                        break;
+                        }
+                }
+
+                if ( device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos" ){
+
+                        pushNotification.register(
+                                successHandler,
+                                errorHandler,
+                                {
+                                        "senderID":"replace_with_sender_id",
+                                        "ecb":"onNotification"
+                                });
+                } else {
+                        pushNotification.register(
+                                tokenHandler,
+                                errorHandler,
+                                {
+                                        "badge":"true",
+                                        "sound":"true",
+                                        "alert":"true",
+                                        "ecb":"onNotificationAPN"
+                                });
+                }                
 
 
         }, false);
