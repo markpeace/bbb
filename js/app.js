796c17aea7eeed834c8ecaf5787ea96652d9aaec
markpeace <mark@learnsomestuff.com>
2014-08-14 18:36:58 +0000

    trying to get messaging working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,54 @@
 if (typeof cordova === 'object') {
         document.addEventListener("deviceready", function() {
                 angular.bootstrap(document, ["bbb"]);        
-        }, false);
+
+                var _pushNotifications = {
+                        initialise: function() {
+                                alert("initialised")
+                                try {
+                                        if ( device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos" ){
+                                                window.plugins.pushNotification.register(
+                                                        _pushNotifications.successHandler,
+                                                        _pushNotifications.errorHandler,
+                                                        {
+                                                                "senderID":"replace_with_sender_id",
+                                                                "ecb":"_pushNotifications.onNotification"
+                                                        });
+                                        } else {
+                                                window.plugins.pushNotification.register(
+                                                        _pushNotifications.tokenHandler,
+                                                        _pushNotifications.errorHandler,
+                                                        {
+                                                                "badge":"true",
+                                                                "sound":"true",
+                                                                "alert":"true",
+                                                                "ecb":"_pushNotifications.onNotification"
+                                                        });
+                                        }
+                                } catch(ex) { alert(ex) }
+                        },
+                        successHandler: function() {
+                                alert("success")
+                        },
+                        errorHandler: function() {
+                                alert("error")
+                        },
+                        tokenHandler: function(result) {
+                                //Parse.User.current().set("token", result).save()  
+                                alert(result)                      
+                        },
+                        onNotification: function() {
+                                alert("onNotification")
+                        },
+                        onNotificationAPN: function() {
+                                alert("onNotificationAPN")
+                        }
+                }        
+
+                _pushNotifications.initialise()
+
+
+                }, false);
 
 } else {        
         angular.element(document).ready(function() {
