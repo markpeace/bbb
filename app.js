01fa9a2124db597c7c33ede49d00de5712559378
markpeace <mark@learnsomestuff.com>
2014-08-15 19:31:35 +0000

    messing with messaging

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 document.addEventListener("deviceready", function() {
-	alert("ready")
+        alert("ready")
+
+        var successHandler = function() {
+                alert("success")
+        }
+        var errorHandler = function() {
+                alert("error")
+        }
+        var tokenHandler = function(result) {
+                alert(result)                
+        }
+
+        var onNotification = function() {
+                alert("gorrit")
+        }
+
+        var initialise = function() {
+
+                window.plugins.pushNotification.register(
+                        tokenHandler,
+                        errorHandler,
+                        {
+                                "badge":"true",
+                                "sound":"true",
+                                "alert":"true",
+                                "ecb":"onNotification"
+                        });
+        }
+
+        initialise();
+
 });
