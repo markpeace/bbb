5d36ba5e52f52797abee5320dfdee5a99cf687c6
markpeace <mark@learnsomestuff.com>
2014-08-15 16:25:52 +0000

    trying push woosh

--- js/app.js
+++ js/app.js
@@ -1,38 +1,46 @@
+function initPushwoosh() {
+        var pushNotification = window.plugins.pushNotification;
+
+        //set push notification callback before we initialize the plugin
+        document.addEventListener('push-notification', function(event) {
+                //get the notification payload
+                var notification = event.notification;
+
+                //display alert to the user for example
+                alert(notification.aps.alert);
+
+                //clear the app badge
+                pushNotification.setApplicationIconBadgeNumber(0);
+        });
+
+        //initialize the plugin
+        pushNotification.onDeviceReady({pw_appid:"3FD7A-0CF27"});
+
+        //register for pushes
+        pushNotification.registerDevice(
+                function(status) {
+                        var deviceToken = status['deviceToken'];
+                        console.warn('registerDevice: ' + deviceToken);
+                        alert('registerDevice: ' + deviceToken);
+                },
+                function(status) {
+                        console.warn('failed to register : ' + JSON.stringify(status));
+                        alert(JSON.stringify(['failed to register ', status]));
+                }
+        );
+
+        //reset badges on app start
+        pushNotification.setApplicationIconBadgeNumber(0);
+}
+
+
 if (typeof cordova === 'object') {
         document.addEventListener("deviceready", function() {
 
                 angular.bootstrap(document, ["bbb"]);        
 
-                var successHandler = function() {
-                        alert("success")
-                }
-                var errorHandler = function() {
-                        alert("error")
-                }
-                var tokenHandler = function(result) {
-                        alert(result)                
-                }
+                initPushwoosh();
 
-                var onNotification = function() {
-                        alert("gorrit")
-                }
-
-                var initialise = function() {
-
-                        alert("Init1")
-
-                        window.plugins.pushNotification.register(
-                                tokenHandler,
-                                errorHandler,
-                                {
-                                        "badge":"true",
-                                        "sound":"true",
-                                        "alert":"true",
-                                        "ecb":"onNotification"
-                                });
-                }
-
-                initialise();
 
         }, false);
 
