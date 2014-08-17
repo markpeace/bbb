1801fd49ae40ee21c3979f2bee7000c08f2ddae6
markpeace <mark@learnsomestuff.com>
2014-08-17 17:43:25 +0000

    sorting pending notifications

--- js/controllers/schedule/messageAttendees.js
+++ js/controllers/schedule/messageAttendees.js
@@ -11,78 +11,50 @@ bbb.controller('MessageAttendees', function($scope, $state, $stateParams, ParseS
         })
 
         $scope.sendMessage = function() {
+
+                $ionicLoading.show({
+                        template: 'Sending Message...'
+                });
+
                 dummyIteration = (new (Parse.Object.extend("Booking")))
                 dummyIteration.id = $scope.id                        
 
                 new Parse.Query("Booking")
                 .equalTo("iteration", dummyIteration)
                 .include("user")
-                .find().then(function(users) {
+                .find().then(function(bookings) {                        
 
-                        angular.forEach(users, function(user) {
+                        secondPromises=[]
 
+                        angular.forEach(bookings, function(booking) {
                                 (new (Parse.Object.extend("Message")))
                                 .save({
                                         from: Parse.User.current(),
-                                        to: user,
+                                        to: booking.get("user"),
                                         message: $scope.message
                                 }).then(function(message) {
-                                        $http({
+
+                                        secondPromises.push ($http({
                                                 method: 'POST',
                                                 url: 'http://mptoolbox.herokuapp.com/pushnotify',
                                                 data: "data=" + JSON.stringify({
-                                                        applicationIndex:1,
+                                                        tokens:[booking.get("user").get("token")],
+                                                        applicationIndex: "1",
                                                         alert:"You have received a message from "+ Parse.User.current().get("forename") + " " + Parse.User.current().get("surname") + " about the '" + $scope.iteration.get("event").get("title") + "' pop-up",
                                                         payload: { messageID: message.id }
                                                 }),
                                                 headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                                        })
-                                        .success(function() {
-                                                $ionicLoading.hide();
-                                                console.log("done")
-                                        })
-                                        .error(function(data, status, headers, config) {
-                                                $ionicLoading.hide();
-                                                alert("An error prevented your message from being sent:" + data)
-                                        });
+                                        }))
+
                                 })
 
                         })
 
-
-                        /*
-
-                        $ionicLoading.show({
-                                template: 'Sending Message...'
-                        });
-
-                        angular.forEach(result, function(r) {
-                                if (r.get("user").get("token")) {$scope.data.tokens.push(r.get("user").get("token"))}
-                        })      
-
-                        $scope.data.applicationIndex="1"
-                        $scope.data.alert = "You have received a message from "+
-                                Parse.User.current().get("forename") + " " +
-                                Parse.User.current().get("surname") +
-                                " about the '" + 
-                                $scope.iteration.get("event").get("title") + "' pop-up"
-
-                        $http({
-                                method: 'POST',
-                                url: 'http://mptoolbox.herokuapp.com/pushnotify',
-                                data: "data=" + JSON.stringify($scope.data),
-                                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                        $q.all(secondPromises).then(function() {
+                                $ionicLoading.hide()
+                                //$state.go("viewEvent", { id:$scope.id })
                         })
-                        .success(function() {
-                                $ionicLoading.hide();
-                                console.log("done")
-                        })
-                        .error(function(data, status, headers, config) {
-                                $ionicLoading.hide();
-                                alert("An error prevented your message from being sent:" + data)
-                        });
 
-			*/
                 })
         }
 
