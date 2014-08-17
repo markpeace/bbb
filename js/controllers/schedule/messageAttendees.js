1164d69789b5beb69b56b38799c608bca2c0c5e3
markpeace <mark@learnsomestuff.com>
2014-08-17 11:54:12 +0000

    functionality to send messages

--- js/controllers/schedule/messageAttendees.js
+++ js/controllers/schedule/messageAttendees.js
@@ -1,43 +1,60 @@
-bbb.controller('MessageAttendees', function($scope, $state, $stateParams, ParseService, $http) { 
+bbb.controller('MessageAttendees', function($scope, $state, $stateParams, ParseService, $ionicLoading, $http) { 
 
         $scope.data = {
-                tokens:[]
+                tokens:[],
+                payload: { message: "" }
         }
 
         $scope.id=$stateParams.id
 
-        dummyIteration = (new (Parse.Object.extend("Booking")))
-        dummyIteration.id = $scope.id                        
-
-
-        new Parse.Query("Booking")
-        .equalTo("iteration", dummyIteration)
-        .include("user")
-        .find().then(function(result) {
-
-                angular.forEach(result, function(r) {
-                        if (r.get("user").get("token")) {$scope.data.tokens.push(r.get("user").get("token"))}
-                })      
-
-                $scope.data.applicationIndex="1"
-                $scope.data.alert = "You have received a message from Mark Peace (host of 'Stupid Questions Amnesty' pop-up)"
-                $scope.data.payload = { test: "mark peace" }
-                
-                $http({
-                        method: 'POST',
-                        url: 'http://mptoolbox.herokuapp.com/pushnotify',
-                        data: "data=" + JSON.stringify($scope.data),
-                        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                })
-                .success(function() {
-                        console.log("done")
-                })
-                .error(function(data, status, headers, config) {
-                        console.log(data)
-                });
-
-
+        new Parse.Query("Iteration")
+        .include("host").include("event")
+        .get($scope.id).then(function(iteration) {
+                $scope.iteration=iteration
         })
+
+        $scope.sendMessage = function() {
+                dummyIteration = (new (Parse.Object.extend("Booking")))
+                dummyIteration.id = $scope.id                        
+
+                new Parse.Query("Booking")
+                .equalTo("iteration", dummyIteration)
+                .include("user")
+                .find().then(function(result) {
+
+                        $ionicLoading.show({
+                                template: 'Sending Message...'
+                        });
+
+                        angular.forEach(result, function(r) {
+                                if (r.get("user").get("token")) {$scope.data.tokens.push(r.get("user").get("token"))}
+                        })      
+
+                        $scope.data.applicationIndex="1"
+                        $scope.data.alert = "You have received a message from "+
+                                Parse.User.current().get("forename") + " " +
+                                Parse.User.current().get("surname") +
+                                " about of the '" + 
+                                $scope.iteration.get("event").get("title") + "' pop-up"
+
+                        $http({
+                                method: 'POST',
+                                url: 'http://mptoolbox.herokuapp.com/pushnotify',
+                                data: "data=" + JSON.stringify($scope.data),
+                                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                        })
+                        .success(function() {
+                                $ionicLoading.hide();
+                                console.log("done")
+                        })
+                        .error(function(data, status, headers, config) {
+                                $ionicLoading.hide();
+                                alert("An error prevented your message from being sent")
+                        });
+
+
+                })
+        }
 
 
 })        
