16d4401031b2adb433b27d4b3ee055ec84d4c3b0
markpeace <mark@learnsomestuff.com>
2014-08-19 15:26:40 +0000

    fails gracefully on Internet Explorer

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
-if (typeof cordova === 'object') {
-        document.addEventListener("deviceready", function() {
-
-                angular.bootstrap(document, ["bbb"]);    
-
-
-
-        });
-
-} else {        
-        angular.element(document).ready(function() {
-                angular.bootstrap(document, ["bbb"]);
-        });
+if (navigator.appName == 'Microsoft Internet Explorer') {
+        alert("Apologies, but the Birley Big Bang app doesn't work very well on Internet Explorer. \n\n Try using an alternative browser such as Chrome or Firefox - or, even better, download our app from the apple or android stores")        
 }
+
+
+        if (typeof cordova === 'object') {
+                document.addEventListener("deviceready", function() {
+
+                        angular.bootstrap(document, ["bbb"]);    
+
+
+
+                });
+
+        } else {        
+                angular.element(document).ready(function() {
+                        angular.bootstrap(document, ["bbb"]);
+                });
+        }
 
 var bbb = angular.module('bbb', ['ionic', 'monospaced.qrcode'])
 
