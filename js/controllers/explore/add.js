dcb90a74bc48739a577b87931072cbabdb1ead0c
markpeace <mark@learnsomestuff.com>
2014-08-20 15:44:47 +0000

    getting geolocation working

--- js/controllers/explore/add.js
+++ js/controllers/explore/add.js
@@ -18,4 +18,25 @@ bbb.controller('AddLocation', function($scope, $state, $stateParams, ParseServic
                         })                        
                 }
         }
+
+        var onSuccess = function(position) {
+                alert('Latitude: '          + position.coords.latitude          + '\n' +
+                      'Longitude: '         + position.coords.longitude         + '\n' +
+                      'Altitude: '          + position.coords.altitude          + '\n' +
+                      'Accuracy: '          + position.coords.accuracy          + '\n' +
+                      'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                      'Heading: '           + position.coords.heading           + '\n' +
+                      'Speed: '             + position.coords.speed             + '\n' +
+                      'Timestamp: '         + position.timestamp                + '\n');
+        };
+
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+                alert('code: '    + error.code    + '\n' +
+                      'message: ' + error.message + '\n');
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
 });
