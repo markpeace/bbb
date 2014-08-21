3d419bd21102963e44f9ddfb3d7ea9aaaf50e8e8
markpeace <mark@learnsomestuff.com>
2014-08-21 09:35:43 +0000

    trying to perfect geolocation accuracy

--- js/controllers/explore/add.js
+++ js/controllers/explore/add.js
@@ -1,51 +1,3 @@
-navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess, geolocationError, geoprogress, options) {
-        var lastCheckedPosition,
-            locationEventCount = 0,
-            watchID,
-            timerID;
-
-        options = options || {};
-
-        var checkLocation = function (position) {
-                lastCheckedPosition = position;
-                locationEventCount = locationEventCount + 1;
-                // We ignore the first event unless it's the only one received because some devices seem to send a cached
-                // location even when maxaimumAge is set to zero
-                if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 1)) {
-                        clearTimeout(timerID);
-                        navigator.geolocation.clearWatch(watchID);
-                        foundPosition(position);
-                } else {
-                        geoprogress(position);
-                }
-        };
-
-        var stopTrying = function () {
-                navigator.geolocation.clearWatch(watchID);
-                foundPosition(lastCheckedPosition);
-        };
-
-        var onError = function (error) {
-                clearTimeout(timerID);
-                navigator.geolocation.clearWatch(watchID);
-                geolocationError(error);
-        };
-
-        var foundPosition = function (position) {
-                geolocationSuccess(position);
-        };
-
-        if (!options.maxWait)            options.maxWait = 10000; // Default 10 seconds
-        if (!options.desiredAccuracy)    options.desiredAccuracy = 20; // Default 20 meters
-        if (!options.timeout)            options.timeout = options.maxWait; // Default to maxWait
-
-        options.maximumAge = 0; // Force current locations only
-        options.enableHighAccuracy = true; // Force high accuracy (otherwise, why are you using this function?)
-
-        watchID = navigator.geolocation.watchPosition(checkLocation, onError, options);
-        timerID = setTimeout(stopTrying, options.maxWait); // Set a timeout that will abandon the location loop
-};
-
 bbb.controller('AddLocation', function($scope, $state, $stateParams, ParseService) { 
 
         $scope.location={
@@ -69,20 +21,34 @@ bbb.controller('AddLocation', function($scope, $state, $stateParams, ParseServic
                 }
         }
 
-        geolocationSuccess = function(e) {
-		$scope.location.geolocation=e.coords
-        }
-        
-        geolocationError = function(e) { alert("error") }
-        
-        geolocationProgress = function(e) {
-		$scope.location.geolocation=$scope.location.geolocation=e.coords.accuracy
-        }
         
         
-        navigator.geolocation.getAccurateCurrentPosition(geolocationSuccess, geolocationError, geolocationProgress)
-
         
 
+        geolocation = {
+                g: this,
+                attempts:0,
+                targetAccuracy:11,
+                onSuccess:function(e) {
+                        
+                        geolocation.attempts++
+                        
+                        $scope.location.geolocation = "Attempt #" +geolocation.attempts+ "/ Accuracy=" +e.coords.accuracy
+                        $scope.$apply()
+                                                
+                        if(e.coords.accuracy<geolocation.targetAccuracy) {                                 
+                                navigator.geolocation.clearWatch(geolocation.watch) 
+                                $scope.location.geolocation=e.coords
+                                $scope.$apply();
+                        }
+                        
+                },
+                onError:function() {},
+                go: function () {
+                        geolocation.watch = navigator.geolocation.watchPosition(geolocation.onSuccess, geolocation.onError, { maximumAge: 3000, maxWait: 10000, enableHighAccuracy: true });
+                }
+        }
+        
+        geolocation.go();
 
 });
