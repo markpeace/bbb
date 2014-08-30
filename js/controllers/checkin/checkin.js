6298892f07b4d76d2eb8a206cf83345e42f3e3cb
markpeace <mark@learnsomestuff.com>
2014-08-30 16:16:01 +0000

    QR Code based on location

--- js/controllers/checkin/checkin.js
+++ js/controllers/checkin/checkin.js
@@ -63,43 +63,9 @@ bbb.controller('CheckIn', function($scope, $state, ParseService, EventModel, $io
                         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
                         scanner.scan( function (result) { 
-
-                                console.log("scanned")
-
-                                dummyIteration = new Parse.Object("Iteration")
-                                dummyIteration.id = result.text
-
-                                new Parse.Query("Booking")
-                                .equalTo("iteration", dummyIteration)
-                                .equalTo("user", Parse.User.current())
-                                .include("checkin")
-
-                                .find().then(function(booking) {
-
-                                        booking=booking[0]
-
-                                        console.log("found booking")
-                                        console.log(booking)
-
-                                        if (booking.get("checkin")) {
-                                                console.log("booking already found")
-                                        } else {
-                                                console.log("booking not found")
-                                                checkin = new Parse.Object("Checkin")
-                                                checkin.save({
-                                                        booking:booking,
-                                                        user: Parse.User.current()
-                                                }).then(function (result) {
-                                                        booking.set("checkin", result).save().then(function() {
-                                                                $scope.$apply()
-                                                        })
-
-                                                })                        
-
-                                        }
+                                $scope.doCheckin EventModel.data().Location.filter(function(location) {
+                                        return location.id==result
                                 })
-
-                                $state.go("tabs.schedule")  
                         })
 
                 } catch (ex) {
--- js/controllers/schedule/eventQRCode.js
+++ js/controllers/schedule/eventQRCode.js
@@ -1,15 +1,10 @@
-bbb.controller('EventQRCode', function($scope, $state,  $stateParams,  $ionicModal, ParseService, $window) { 
+bbb.controller('EventQRCode', function($scope, $state,  $stateParams,  $ionicModal, ParseService, $window, EventModel) { 
 
         $scope.width= $window.innerWidth<$window.innerHeight ? $window.innerWidth : $window.innerHeight * .9
         $scope.width = $scope.width *.9
         
-        var Iteration = Parse.Object.extend("Iteration");
-        var query = new Parse.Query(Iteration);
-        query.include("event").include("event.series").include("host").include("location");
-        query.get($stateParams.id, {})
-        .then(function (results) {
-                $scope.iteration=results
-                $scope.$apply();                        
-        })
+        $scope.iteration = EventModel.data().Iteration.filter(function(iteration) {         
+                return iteration.id==$stateParams.id
+        })[0] || null
 
 })        
