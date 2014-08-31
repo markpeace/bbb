e71c7726c42e7aee88c477050a2be68020deeb9f
markpeace <mark@learnsomestuff.com>
2014-08-31 13:47:31 +0000

    register now appropriately connected to checkins

--- js/controllers/schedule/register.js
+++ js/controllers/schedule/register.js
@@ -1,4 +1,4 @@
-bbb.controller('AttendenceRegister', function($scope, ParseService, $stateParams) { 
+bbb.controller('AttendenceRegister', function($scope, ParseService, $stateParams, $q) { 
 
         $scope.bookings = []
         $scope.id=$stateParams.id
@@ -15,32 +15,40 @@ bbb.controller('AttendenceRegister', function($scope, ParseService, $stateParams
         .include("iteration").include("iteration.location")
         .find().then(function(result) {
 
+                $scope.bookings = result                   
+
                 result.forEach(function(booking) {
-                        booking.attended=false
+                        new Parse.Query("Checkin")
+                        .equalTo("iteration", dummyIteration)
+                        .equalTo("user", booking.get('user'))                        
+                        .limit(1).find().then(function(result) {                                
+                                booking.checkin=result[0] || null   
+                                $scope.$apply();
+                        })
                 })
-                
-                $scope.bookings = result                
-                
-                $scope.$apply()
+
         })
 
 
         $scope.toggleAttendence = function(booking) {
-                
-                console.log(booking.get("iteration"))
-                
-		if (booking.attended) {
-                        console.log("delete attendence record")
+
+                if (booking.checkin) {
+                        booking.checkin.destroy().then(function(){
+                                booking.checkin=null;
+                                $scope.$apply()
+                        })
                 } else {
-                        
+
                         (new (Parse.Object.extend("Checkin")))
                         .save({
                                 iteration: dummyIteration,
-                                location: booking.get("iteration").get("location")
-                        })
-                        
-                        
-                        console.log("add attendence record")
+                                location: booking.get("iteration").get("location"),
+                                user: booking.get('user')
+                        }).then(function(checkin) {
+                                booking.checkin=checkin
+                                $scope.$apply()
+                        }) 
+
                 }       
         }
 
