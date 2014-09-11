dcbb9b9cbfe7f9e1dcc4afcc22e91f35be66fc2e
markpeace <mark@learnsomestuff.com>
2014-09-11 19:01:16 +0000

    fixed date bug

--- js/services/eventModel.js
+++ js/services/eventModel.js
@@ -275,12 +275,14 @@ bbb.factory('EventModel', ["NotificationService","ParseService", "$ionicLoading"
                                         dummyIteration = (new (Parse.Object.extend("Iteration")))
                                         dummyIteration.id = iteration.id;        
 
-                                        alert("about to submit")
+                                        alert("about to submit");
 
                                         (new (Parse.Object.extend("Booking")))	
                                         .save({user:Parse.User.current(), iteration:dummyIteration}).then(function() {
                                                 $ionicLoading.hide();
                                         }) 
+                                        
+                                        alert("submitted");
                                         iteration.bookings++;
                                         cache.save();        
 
